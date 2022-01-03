689d96edaa9a81cf12c48ff1d32224cc78fb34f9
Arash Mehdipour <arash201221@gmail.com>
2022-01-03 20:44:23 +0330

    Add Onconected Method Add js SignalR

---
+++ SignalRProject/Hubs/SiteChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRProject.Hubs
+{
+    public class SiteChatHub : Hub
+    {
+        public async Task SendMessage(string sender, string message)
+        {
+            await Clients.All.SendAsync("GetNewMessage", sender, message, DateTime.Now.ToShortDateString());
+        }
+
+        public override Task OnConnectedAsync()
+        { 
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- SignalRProject/Startup.cs
+++ SignalRProject/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SignalRProject.Hubs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -52,6 +53,7 @@ namespace SignalRProject
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapHub<SiteChatHub>("/chathub");
             });
         }
     }
